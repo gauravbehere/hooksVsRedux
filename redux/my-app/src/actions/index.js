f63b7fb33e774fa0cdca3dd344e9e1a33dda5df5
Gaurav <Gaurav.techgeek@gmail.com>
2020-10-12 10:22:35 +0530

    Create index.js

---
+++ redux/my-app/src/actions/index.js
@@ -0,0 +1,13 @@
+export const addTodo = ({ id, text }) => ({
+    type: "ADD_TODO",
+    payload: {
+      id,
+      text,
+    },
+  });
+  
+  export const doneTodo = id => ({
+    type: "DONE_TODO",
+    payload: id,
+  });
+  
