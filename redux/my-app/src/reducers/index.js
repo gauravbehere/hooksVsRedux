c112d406d6661ab53c696f7b87789fdd3b56c4fc
Gaurav <Gaurav.techgeek@gmail.com>
2020-10-12 10:21:06 +0530

    Create index.js

---
+++ redux/my-app/src/reducers/index.js
@@ -0,0 +1,25 @@
+const initState = {
+    todos: [
+      {
+        id: "0",
+        text: "Learn React hooks",
+      },
+    ],
+  };
+  
+  const todos = (state = initState, action) => {
+    switch (action.type) {
+      case "ADD_TODO":
+        return { todos: [...state.todos, action.payload] };
+      case "DONE_TODO":
+        return {
+          todos: state.todos.filter(todo => todo.id !== action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export {
+      todos, initState
+  };
